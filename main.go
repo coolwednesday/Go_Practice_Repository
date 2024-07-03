84149d72c1005a7a8d566db01240479ebdf2b7c0
Raramuri <raramuri@172-0-189-101.lightspeed.tukrga.sbcglobal.net>
2024-07-03 17:58:20 +0530

    Called few of the implemented functions in the question in the main.go file

--- main.go
+++ main.go
@@ -5,5 +5,8 @@ import "fmt"
 func main() {
 
 	fmt.Println("Hello World")
+	fmt.Println("Perimeter of rectangle is :", PerimeterRectangle(2, 3))
+	fmt.Println("Circumference of Circle is:", PerimeterCircle(4))
+	fmt.Println("Bob Replied : ", Hey("WHAT IS THE STATUS ?"))
 
 }
