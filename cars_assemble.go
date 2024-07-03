d2490f6c31d4b0005b2533fb9e77a2074484d50c
Raramuri <raramuri@172-0-189-101.lightspeed.tukrga.sbcglobal.net>
2024-07-03 15:46:28 +0530

    Basic Prgrams of Go, involving datatypes, type conversion, bool comparison, string manipulation and condition

---
+++ cars_assemble.go
@@ -0,0 +1,23 @@
+package main
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * successRate * 0.01
+	panic("CalculateWorkingCarsPerHour not implemented")
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int((float64(productionRate) / 60.0) * successRate * 0.01)
+	panic("CalculateWorkingCarsPerMinute not implemented")
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	a := carsCount / 10
+	b := carsCount % 10
+	return uint(a*95000 + b*10000)
+	panic("CalculateCost not implemented")
+}
---
+++ perimeter.go
@@ -0,0 +1,9 @@
+package main
+
+func PerimeterCircle(x float64) float64 {
+	return 2 * 3.14 * x
+}
+
+func PerimeterRectangle(x float64, y float64) float64 {
+	return 2 * (x + y)
+}
