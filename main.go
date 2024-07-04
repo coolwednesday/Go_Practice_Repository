a8add05d7b9bd5e71e1550217325a277ee190229
Raramuri <raramuri@172-0-189-101.lightspeed.tukrga.sbcglobal.net>
2024-07-04 12:56:39 +0530

    Added testing and program to find the count of prime numbers till a given Number

--- main.go
+++ main.go
@@ -8,5 +8,6 @@ func main() {
 	fmt.Println("Perimeter of rectangle is :", PerimeterRectangle(2, 3))
 	fmt.Println("Circumference of Circle is:", PerimeterCircle(4))
 	fmt.Println("Bob Replied : ", Hey("WHAT IS THE STATUS ?"))
+	fmt.Println("Number of Prime Numbers: ", PrimeNumbers(0))
 
 }
