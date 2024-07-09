90db0e83b5014ccc12abf019bdc0295ad28a8b94
Raramuri <raramuri@172-0-189-101.lightspeed.tukrga.sbcglobal.net>
2024-07-09 17:46:29 +0530

    Fixed edge test cases for shape_test.go

--- shape.go
+++ shape.go
@@ -29,34 +29,58 @@ type square struct {
 }
 
 func (s square) area() float64 {
+	if s.length < 0 {
+		return -1
+	}
 	return s.length * s.length
 }
 
 func (s square) perimeter() float64 {
+	if s.length < 0 {
+		return -1
+	}
 	return 4 * s.length
 }
 
 func (r rectangle) area() float64 {
+	if r.length < 0 || r.breadth < 0 {
+		return -1
+	}
 	return r.length * r.breadth
 }
 
 func (r rectangle) perimeter() float64 {
+	if r.length < 0 || r.breadth < 0 {
+		return -1
+	}
 	return 2 * (r.length + r.breadth)
 }
 
 func (c circle) perimeter() float64 {
+	if c.radius < 0 {
+		return -1
+	}
 	return 2 * pi * c.radius
 }
 
 func (c circle) area() float64 {
+	if c.radius < 0 {
+		return -1
+	}
 	return pi * c.radius * c.radius
 }
 
 func (t triangle) perimeter() float64 {
+	if t.base < 0 || t.height < 0 {
+		return -1
+	}
 	return t.base + t.height + math.Sqrt(t.base*t.base+t.height*t.height)
 }
 
 func (t triangle) area() float64 {
+	if t.base < 0 || t.height < 0 {
+		return -1
+	}
 	return 0.5 * t.base * t.height
 }
 
